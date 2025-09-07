dd876f07c4486e5e6509c9d96cf83aed39242ac2
Nilesh Gunvant Mahajan <135039225+nileshgmahajan@users.noreply.github.com>
2025-09-07 10:37:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (20.85%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
@@ -2,15 +2,15 @@ class Solution {
     public int[] sumZero(int n) {
         int []  result=new int[n];
         int index=0;
-        if(n % 2 ==1) 
+        if(n % 2 ==1) //odd condition
         {
-            result[index++] =0; //add the 0 if odd
+            result[index++] =0; //add the 0 if odd and also increment the index
         } 
 
         for(int x=1;index<n;x++)
         {
-            result[index++]= x;//place the positive element 
-            result[index++]=-x;//place the negative element
+            result[index++]= x;//place the positive element and al0 increment the index 
+            result[index++]=-x;//place the negative element and also incremant the index
         }
         return result;
         
