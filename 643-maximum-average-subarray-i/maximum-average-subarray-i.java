baa7309622354e3d2cb8fead047117209ce4b2b1
Nilesh Gunvant Mahajan <135039225+nileshgmahajan@users.noreply.github.com>
2025-11-03 23:44:43 +0530

    Time: 4 ms (62.91%) | Memory: 69 MB (5.16%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -3,14 +3,18 @@ class Solution {
 
         int n=nums.length;
         double currentWindowSum=0;
+        //sum of the current window
         for(int i=0;i< k ;i++)
         {
             currentWindowSum +=nums[i];
         }
         double maxsum=currentWindowSum;
 
+
+ ///
         for(int i=k;i<n;i++)
         {
+            //add the new element in window and remove the first window element
             currentWindowSum +=nums[i] - nums[i - k]; 
             maxsum =Math.max(maxsum,currentWindowSum);
         }
