2760d668ab481a7baa6526303f766bce6cb32df8
Nilesh Gunvant Mahajan <135039225+nileshgmahajan@users.noreply.github.com>
2026-03-25 11:55:22 +0530

    Time: 1 ms (70.36%) | Memory: 47.1 MB (94.16%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,40 @@
+class Solution {
+     // Function to set entire row and column to 0 if an element in the matrix is 0
+    public void setZeroes(int[][] matrix) {
+        //get number of row
+        int m=matrix.length;
+        //get number column
+        int n=matrix[0].length;
+        // Create row marker array
+        boolean[] row=new  boolean[m];
+         // Create column marker array
+        boolean []col=new boolean[n];
+
+        // First pass: mark rows and columns that need to be zeroed
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                 // If element is zero, mark its row and column
+                if(matrix[i][j] == 0)
+                {
+                    row[i]=true;
+                    col[j]=true;
+                }
+            }
+        }
+           // Second pass: set cells to zero based on markers
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                // If the row or column is marked, set cell to zero
+                if(row[i] || col[j] )
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        
+    }
+}
